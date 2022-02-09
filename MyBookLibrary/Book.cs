2ba9c376f1fe894b5a178098757918cb3b9f17b8
PVAK244 <khoapvade150321@fpt.edu.vn>
2022-02-09 16:22:29 +0700

    Add project files.

---
+++ MyBookLibrary/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyBookLibrary
+{
+    public class Book
+    {
+        private int id;
+        private string name;
+        private string publisher;
+        private double price;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Publisher { get => publisher; set => publisher = value; }
+        public double Price { get => price; set => price = value; }
+
+        public Book(int id, string name, string publisher, double price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Publisher = publisher;
+            this.Price = price;
+        }
+
+        public Book()
+        {
+        }
+
+        override
+        public String ToString()
+        {
+            return $"ID: {id}, Name: {Name}, Publisher: {publisher}, Price: {price}";
+        }
+    }
+}
---
+++ MyBookLibrary/ListBook.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyBookLibrary
+{
+    public class ListBook
+    {
+        private List<Book> books;
+
+        public ListBook()
+        {
+            books = new List<Book>();
+        }
+
+        public Book existID(int id)
+        {
+            foreach(Book book in books)
+            {
+                if(book.Id == id) return book;
+            }
+            return null;
+        }
+
+        public void AddBook(int id, string name, string publisher, double price)
+        {
+            if (existID(id) == null)
+            {
+                books.Add(new Book(id, name, publisher, price));
+                Console.WriteLine("Status of the action: sucess");
+            }
+            else Console.WriteLine("Status of the action: failure\nId exist");
+        }
+
+        public void Update(int id, string name, string publisher, double price)
+        {
+            Book b = new Book(id, name, publisher, price);
+            foreach(Book book in books)
+            {
+                book.Name = name;
+                book.Publisher = publisher;
+                book.Price = price;
+            }
+            Console.WriteLine("Status of the action: sucess");
+        }
+
+        public void Delete(int id)
+        {
+            Book book = existID(id);
+            if (book != null)
+            {
+                books.Remove(book);
+                Console.WriteLine("Status of the action: sucess");
+            }
+            else Console.WriteLine("Status of the action: failure\nId do not exist");
+        }
+
+
+        public void PrintAll()
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book);
+            }
+        }
+    }
+}
